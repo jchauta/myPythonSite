5d0ce4d4726e0277141ec67b08e3694b8ac10d0c
jchauta <juanxxcb@hotmail.com>
2015-12-18 13:26:30 -0500

    Commit 2
    Second

--- polls/views.py
+++ polls/views.py
@@ -9,3 +9,14 @@ def index(request):
 
 def juanpage(request):
 	return HttpResponse("Hi JUAN!, welcome back!")
+
+def detail(request,question_id):
+	return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
+

f574fafc310face63405826c45e545387476f715
Juan Chauta <juanxxcb@hotmail.com>
2016-01-13 14:42:12 -0500

    Check in - Wednesday

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse , Http404
+from django.template import loader
 # Create your views here.
 
+from .models import Question
 
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list':latest_question_list,
+	}
+	return HttpResponse(template.render(context,request))
 
 def juanpage(request):
 	return HttpResponse("Hi JUAN!, welcome back!")
 
-def detail(request,question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, question_id):
+	
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question })
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
